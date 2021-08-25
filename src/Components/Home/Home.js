dbef8072dc2bfde4fb3747e51545661cc20ea022
ketanmalik <ketanmalik@gmail.com>
2021-08-25 02:26:43 -0400

    adding dark mode switch

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Particles from 'react-particles-js';
+import particlesConfig from '../../Assets/particlesConfig.json';
 
 class Home extends Component {
     render() {
         return <div>
+            <Particles params={particlesConfig} />
             <Link to="/about">About</Link>
             <br></br>
             <Link to="/skills">Skills</Link>
